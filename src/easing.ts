1c9afb0bfcdb3c5968fc8da4a600fbc416bae816
Daniele Borra <daniele.borra.93@gmail.com>
2019-07-28 22:19:06 +0200

    Fix easing names

--- src/easing.ts
+++ src/easing.ts
@@ -1,9 +1,9 @@
 const Easings = {
   LINEAR: (t : number) => t,
-  OUT_QUAD: (t : number) => t*(2-t),
-  OUT_CUBIC: (t : number) => (--t)*t*t+1,
-  OUT_QUART: (t : number) => 1-(--t)*t*t*t,
-  OUT_QUINT: (t : number) => 1+(--t)*t*t*t*t,
+  QUAD: (t : number) => t*(2-t),
+  CUBIC: (t : number) => (--t)*t*t+1,
+  QUART: (t : number) => 1-(--t)*t*t*t,
+  QUINT: (t : number) => 1+(--t)*t*t*t*t,
 }
 
 export default Easings;
