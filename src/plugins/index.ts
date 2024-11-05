8626ccdc28ff77ffcce3fd50c469e8df6956bd97
Daniele <daniele@adoratorio.com>
2024-11-05 14:18:03 +0100

    Added plugins export

---
+++ src/plugins/index.ts
@@ -0,0 +1,12 @@
+import CSSRender from "./css-render";
+import TargetsDetection from "./targets-detection";
+
+const plugins = {
+  CSSRender,
+  TargetsDetection,
+};
+
+export { CSSRender };
+export { TargetsDetection };
+
+export default plugins;
